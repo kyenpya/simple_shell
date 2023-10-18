fee73b53351bd9b7afb585c49fe317cc61866b91
teslim201 <teslim201@gmail.com>
2023-10-18 14:54:33 -0700

    shell

--- 4builtins_list.c
+++ 4builtins_list.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * builtins_list -this search for match and execute
- * the associate builtin
+ * builtins_list -this searches for a match and executes
+ * the associate builtin.
  * @data: the struct for the program's data
- * Return: this Returns the return of the function
- * executed is there is a match,
- * otherwise only returns -1.
+ * Return: this Returns the return of the executed function
+ * execute if there is a match,
+ * otherwise only return -1.
  **/
 int builtins_list(data_of_program *data)
 {
@@ -21,17 +21,16 @@ int builtins_list(data_of_program *data)
 		{"unsetenv", builtin_unset_env},
 		{NULL, NULL}
 	};
-
-/* this walks through the structure */
-for (iterator = 0; options[iterator].builtin != NULL; iterator++)
+	/* this walks through the structure */
+	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
+	{
+		/* prints if there is a match between the given command and a builtin, */
+		if (str_compare(options[iterator].builtin, data->command_name, 0))
 		{
-/* prints if there is a match between the given command and a builtin, */
-if (str_compare(options[iterator].builtin, data->command_name, 0))
-			{
-/* this execute the function, and return the return value of the function */
+			/* this execute the function, and return the return value of the function */
 			return (options[iterator].function(data));
-			}
-/*prints if there is no match return -1 */
 		}
+		/*prints if there is no match return -1 */
+	}
 	return (-1);
 }
